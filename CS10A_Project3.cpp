c36615bafc065f452efba9c29b54c89819389e7d
Joel Thomas <broelgadude@gmail.com>
2020-04-24 13:55:37 -0700

    Update CS10A_Project3.cpp

--- CS10A_Project3.cpp
+++ CS10A_Project3.cpp
@@ -1,11 +1,10 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                      Project 3 : 1 - D array processing : Score Analysis program
-// This program gives the user a menu with various options. It allows for score inputs into an array
-// with the ability to input scores, read scores from file, sort the list and search for a specific score.
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                      Project 3 : 1 - D array processing : Score Analysis program                        //
+// This program gives the user a menu with various options. It allows for score inputs into an array       //
+// with the ability to input scores, read scores from file, sort the list and search for a specific score. //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Function prototypes
-unsigned programMenu(int[], int);         // Function called from main(), calls all other functions depending on user choice.
 void getScores(int[], int);               // Function that allows user to input scores.
 void readScores(int[], int);              // Function that reads scores from file.
 void printScores(const int[], int);       // Function that prints all scores.
@@ -28,11 +27,89 @@ int main()
     // Declare variables
     const int ARRAY_SIZE = 10;    // The size of the array
     int scores[ARRAY_SIZE] = {0}; // The array, initialized with 0
-    bool running = true;          // Boolean to keep program running while true
+    unsigned choice = 0;          // User choice for option
+    bool running = true,          // Boolean to keep program running while true
+        isValid;                  // Used for input verification
 
     /* Begin program loop. Bool is returned once function is complete */
     while (running)
-        running = programMenu(scores, ARRAY_SIZE);
+    {
+
+        // Display menu and get input
+        cout << "---------------------------------------------------\n"
+             << "1-D ARRAY PROCESSING MENU OPTIONS\n"
+             << "---------------------------------------------------\n"
+             << "1. Read in 10 scores from user.\n"
+             << "2. Read in 10 scores from the file, scores.txt.\n"
+             << "3. Print all scores.\n"
+             << "4. Print the highest score.\n"
+             << "5. Print the lowest score.\n"
+             << "6. Print the average score.\n"
+             << "7. Print one score (give its entry number)\n"
+             << "8. Quit program\n"
+             << "9. Sort scores in descending order.\n"
+             << "10. Search for score.\n\n";
+        do
+        {
+            isValid = true;
+
+            cout << "Enter your choice: ";
+            cin >> choice;
+            cin.ignore(100, '\n');
+            cout << "\n";
+
+            // Switch statement for control, also verifies input
+            if (choice > 0 && choice < 11)
+                switch (choice)
+                {
+                case 1: /* Read 10 scores from user */
+                    getScores(scores, ARRAY_SIZE);
+                    break;
+                case 2: /* Read 10 scores from file */
+                    readScores(scores, ARRAY_SIZE);
+                    break;
+                case 3: /* Print all scores */
+                    printScores(scores, ARRAY_SIZE);
+                    break;
+                case 4: /* Print the highest score */
+                    printHighestScore(scores, ARRAY_SIZE);
+                    break;
+                case 5: /* Print the lowest score */
+                    printLowestScore(scores, ARRAY_SIZE);
+                    break;
+                case 6: /* Print the average score */
+                    printAverageScore(scores, ARRAY_SIZE);
+                    break;
+                case 7: /* Print one score (give its entry number) ((Call specific subscript)) */
+                    printOneScore(scores, ARRAY_SIZE);
+                    break;
+                case 8: /* Quit the program */
+                    cout << "Array processing test now concluded. Exiting program .....\n";
+                    running = false; // Program terminates
+                    break;
+                case 9: /* Sort scores in descending order */
+                    sortDescending(scores, ARRAY_SIZE);
+                    cout << "Sort completed.\n"
+                         << "Please select the print all scores menu option to view the scores.\n"
+                         << "\nPress enter to continue ";
+                    cin.get();
+                    break;
+                case 10: /* Allow user to search for score */
+                    searchForValue(scores, ARRAY_SIZE);
+                    break;
+                }
+            else // Invalid choice-restart loop
+            {
+                if (!cin)
+                {
+                    cin.clear();
+                    cin.ignore(100, '\n');
+                }
+                cout << "Invalid choice. Please choose options 1 - 10.\n\n";
+                isValid = false;
+            }
+        } while (!isValid); // while choice is invalid
+    }
 }
 
 /////////////////////////////
@@ -40,110 +117,17 @@ int main()
 /////////////////////////////
 
 /////////////////////////////////////////////////////////////////////
-// Function is the main program. All other function calls are
-// from this function. The user will be prompted with a menu and
-// given options. The program will call the function that is chosen
-// by user. After function has comepleted, it ill return to main()
-// where the status of running will be checked. If running is true,
-// this function will be called again from main().
-/////////////////////////////////////////////////////////////////////
-unsigned programMenu(int someArray[], int limit)
-{
-    // Pre:     Prompt user with menu options.
-    // Post:    Uses switch statement to call specified option. Returns to main() when complete.
-    // Declare variables
-    unsigned choice = 0;
-    bool isValid; // Bool to return to main(). If user has not chosen to quit, program will restart.
-
-    // Display menu and get input
-    cout << "---------------------------------------------------\n"
-         << "1-D ARRAY PROCESSING MENU OPTIONS\n"
-         << "---------------------------------------------------\n"
-         << "1. Read in 10 scores from user.\n"
-         << "2. Read in 10 scores from the file, scores.txt.\n"
-         << "3. Print all scores.\n"
-         << "4. Print the highest score.\n"
-         << "5. Print the lowest score.\n"
-         << "6. Print the average score.\n"
-         << "7. Print one score (give its entry number)\n"
-         << "8. Quit program\n"
-         << "9. Sort scores in descending order.\n"
-         << "10. Search for score.\n\n";
-    do
-    {
-        isValid = true;
-
-        cout << "Enter your choice: ";
-        cin >> choice;
-        cin.ignore(100, '\n');
-        cout << "\n";
-
-        // Switch statement for control, also verifies input
-        if (choice > 0 && choice < 11)
-            switch (choice)
-            {
-            case 1: /* Read 10 scores from user */
-                getScores(someArray, limit);
-                break;
-            case 2: /* Read 10 scores from file */
-                readScores(someArray, limit);
-                break;
-            case 3: /* Print all scores */
-                printScores(someArray, limit);
-                break;
-            case 4: /* Print the highest score */
-                printHighestScore(someArray, limit);
-                break;
-            case 5: /* Print the lowest score */
-                printLowestScore(someArray, limit);
-                break;
-            case 6: /* Print the average score */
-                printAverageScore(someArray, limit);
-                break;
-            case 7: /* Print one score (give its entry number) ((Call specific subscript)) */
-                printOneScore(someArray, limit);
-                break;
-            case 8: /* Quit the program */
-                cout << "Array processing test now concluded. Exiting program .....\n";
-                return 0; // Program terminates
-            case 9: /* Sort scores in descending order */
-                sortDescending(someArray, limit);
-                cout << "Sort completed.\n"
-                     << "Please select the print all scores menu option to view the scores.\n"
-                     << "\nPress enter to continue ";
-                cin.get();
-                break;
-            case 10: /* Allow user to search for score */
-                searchForValue(someArray, limit);
-                break;
-            }
-        else // Invalid choice-restart loop
-        {
-            if (!cin)
-            {
-                cin.clear();
-                cin.ignore(100, '\n');
-            }
-            cout << "Invalid choice. Please choose options 1 - 8.\n\n";
-            isValid = false;
-        }
-    } while (!isValid); // while choice is invalid
-
-    return 2; // Program restarts
-}
-
-/////////////////////////////////////////////////////////////////////
 // Function that allows user to input scores.
 // User is prompted for scores input. Each iteration of the loop
 // will assign the input to an array subscript. The input is
 // verified. If verification fails, user will be prompted to input
 // data again. Once completed function will return to
-// programMenu().
+// main().
 /////////////////////////////////////////////////////////////////////
 void getScores(int someArray[], int limit)
 {
     // Pre:     User is prompted to input scores one at a time.
-    // Post:    Once all scores have been input, function returns to programMenu().
+    // Post:    Once all scores have been input, function returns to main().
     // Declare variables
     bool isValid;
     unsigned userInput = 0;
@@ -195,12 +179,12 @@ void getScores(int someArray[], int limit)
 // from. If file is correct, the data is read from file and stored
 // in array. Else the user is prompted to provide correct filename.
 // File is closed, reset to begining and function returns to
-// programMenu().
+// main().
 /////////////////////////////////////////////////////////////////////
 void readScores(int someArray[], int limit)
 {
     // Pre:     Prompt for and get filename.
-    // Post:    Verify correct filename and read data and store into array. Return to programMenu().
+    // Post:    Verify correct filename and read data and store into array. Return to main().
     // Declare variables
     string filename;
     ifstream inFile;
@@ -253,12 +237,12 @@ void readScores(int someArray[], int limit)
 /////////////////////////////////////////////////////////////////////
 // Function that prints all scores.
 // Function will index through array[] and print scores with every
-// iteration. Returns to programMenu().
+// iteration. Returns to main().
 /////////////////////////////////////////////////////////////////////
 void printScores(const int someArray[], int limit)
 {
     // Pre:     Starts at array[0] and prints first score.
-    // Post:    Once all scores have printed, returns to programMenu().
+    // Post:    Once all scores have printed, returns to main().
     // Begin function loop.
     for (unsigned index = 0; index < limit; index++)
     {
@@ -278,7 +262,7 @@ void printHighestScore(const int someArray[], int limit)
 {
     // Pre:     Iterate through array and compare score to highest variable.
     // Post:    Once all scores have been compared and the highest score has been assigned,
-    //          score is displayed to user. Returns to programMenu().
+    //          score is displayed to user. Returns to main().
     // Declare variable
     unsigned highest = 0;
 
@@ -303,7 +287,7 @@ void printLowestScore(const int someArray[], int limit)
 {
     // Pre:     Iterate through array and compare score to lowest variable.
     // Post:    Once all scores have been compared and the lowest score has been assigned,
-    //          score is displayed to user. Returns to programMenu().
+    //          score is displayed to user. Returns to main().
     // Declare variable
     unsigned lowest = 1000;
 
@@ -323,12 +307,12 @@ void printLowestScore(const int someArray[], int limit)
 // Function that prints the average score.
 // The function will iterate through the array and accumulate the
 // scores. Once competed the mean is calculated and displayed to the
-// user. Returns to programMenu().
+// user. Returns to main().
 /////////////////////////////////////////////////////////////////////
 void printAverageScore(const int someArray[], int limit)
 {
     // Pre:     Iterate through scores and accumulate each score.
-    // Post:    cast the score and assign the average to variable. Display average and return to programMenu().
+    // Post:    cast the score and assign the average to variable. Display average and return to main().
     // Declare variables
     unsigned sum = 0;
     float average;
@@ -348,15 +332,74 @@ void printAverageScore(const int someArray[], int limit)
 }
 
 /////////////////////////////////////////////////////////////////////
+// Function that allows user to print a specific score and displays
+// the number of scores that are higher.
+// User is prompted for entry number of score they want to view.
+// Input is verified before continuing. Entry number and entry score
+// is displayed then the array is iterated to find the numbers
+// that are higher than that score. Any higher scores are counted
+// then displayed. Returns to main().
+/////////////////////////////////////////////////////////////////////
+void printOneScore(const int someArray[], int limit)
+{
+    // Pre:     Get entry number from user and verify.
+    // Post:    Display entry number and score display. Iterate thru array and count number of higher scores.
+    //          Display higher counter and return to main().
+    // Declare variables
+    unsigned entryNum,
+        higherCounter = 0;
+    bool isValid = false;
+
+    do
+    {
+        isValid = true;
+        // Prompt for, get and verify entry number.
+        cout << "Please enter entry or row # of score you want: ";
+        cin >> entryNum;
+        cin.ignore(100, '\n');
+
+        // Input verification
+        if (cin)
+        {
+            if (entryNum > 0 && entryNum <= limit)
+                isValid = true;
+            else
+            {
+                cout << "Invalid input. Try again.\n";
+                isValid = false;
+            }
+        }
+        else
+        {
+            cout << "Invalid input. Try again.\n";
+            cin.clear();
+            cin.ignore(100, '\n');
+            isValid = false;
+        }
+    } while (!isValid);
+
+    cout << "\nEntry #" << entryNum << " score: " << someArray[(entryNum - 1)];
+
+    for (unsigned index = 0; index < limit; index++)
+    {
+        if (someArray[index] > someArray[(entryNum - 1)])
+            higherCounter++;
+    }
+    cout << "\nScore statistics: " << higherCounter << " higher.\n"
+         << "\nPress enter to continue ";
+    cin.get();
+}
+
+/////////////////////////////////////////////////////////////////////
 // Function that sorts the scores in descending order.
 // Function uses selection sort algorithm to sort the scores in the
 // array in descending order in conjuction with swap() function.
-// Returns to programMenu().
+// Returns to main().
 /////////////////////////////////////////////////////////////////////
 void sortDescending(int someArray[], int limit)
 {
     // Pre:     Declare variables and begin selection sort loop.
-    // Post:    Array is sorted in descending order and returns to programMenu().
+    // Post:    Array is sorted in descending order and returns to main().
     // Declare variable.
     int maxIndex,
         maxValue;
@@ -387,13 +430,13 @@ void sortDescending(int someArray[], int limit)
 // User is prompted to input score. User-score is verified and sent to
 // binarySearch() to find score. binarySearch() returns with either
 // the index of the score or -1 if score was not found. Data is
-// displayed to user and returns to programMenu().
+// displayed to user and returns to main().
 /////////////////////////////////////////////////////////////////////h
 void searchForValue(int someArray[], int limit)
 {
     // Pre:     Prompt for, get, and verify input. Send input to binarySearch().
     // Post:    binarySearch() returns value, either the index of the score or -1 if not found.
-    //          Data is displayed to user and returns to programMenu().
+    //          Data is displayed to user and returns to main().
     // Declare variables.
     unsigned userScore = 0;
     bool isValid = false;
@@ -486,65 +529,6 @@ int binarySearch(int someArray[], int limit, int value)
         return position;
     else
         return middle;
-}
-
-/////////////////////////////////////////////////////////////////////
-// Function that allows user to print a specific score and displays
-// the number of scores that are higher.
-// User is prompted for entry number of score they want to view.
-// Input is verified before continuing. Entry number and entry score
-// is displayed then the array is iterated to find the numbers
-// that are higher than that score. Any higher scores are counted
-// then displayed. Returns to programMenu().
-/////////////////////////////////////////////////////////////////////
-void printOneScore(const int someArray[], int limit)
-{
-    // Pre:     Get entry number from user and verify.
-    // Post:    Display entry number and score display. Iterate thru array and count number of higher scores.
-    //          Display higher counter and return to programMenu().
-    // Declare variables
-    unsigned entryNum,
-        higherCounter = 0;
-    bool isValid = false;
-
-    do
-    {
-        isValid = true;
-        // Prompt for, get and verify entry number.
-        cout << "Please enter entry or row # of score you want: ";
-        cin >> entryNum;
-        cin.ignore(100, '\n');
-
-        // Input verification
-        if (cin)
-        {
-            if (entryNum > 0 && entryNum <= limit)
-                isValid = true;
-            else
-            {
-                cout << "Invalid input. Try again.\n";
-                isValid = false;
-            }
-        }
-        else
-        {
-            cout << "Invalid input. Try again.\n";
-            cin.clear();
-            cin.ignore(100, '\n');
-            isValid = false;
-        }
-    } while (!isValid);
-
-    cout << "\nEntry #" << entryNum << " score: " << someArray[(entryNum - 1)];
-
-    for (unsigned index = 0; index < limit; index++)
-    {
-        if (someArray[index] > someArray[(entryNum - 1)])
-            higherCounter++;
-    }
-    cout << "\nScore statistics: " << higherCounter << " higher.\n"
-         << "\nPress enter to continue ";
-    cin.get();
 }
 
 /////////////////////////////////////////////////////////////////////
