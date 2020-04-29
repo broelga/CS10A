4db609100301c42dbbd4f68fbbb9fc4720c738fd
Joel Thomas <broelgadude@gmail.com>
2020-04-29 12:01:07 -0700

    Add files via upload

---
+++ Thomas_P4a.cpp
@@ -0,0 +1,117 @@
+/*
+Name: Joel Thomas
+Class: CS10A
+Date: April 27, 2020
+Instructor: Sujan Sarkar
+Filename: Thomas_p4a.cpp
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Project 4a: Simple array manipulations                                                             //
+// This program is used to sort and display arrays in different way. An array is copied from another  //
+// array and is displayed in rotated order, and then in reverse order of the original array.          //
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+void showArray(/* in */ int[], /* in */ int);                             // Function to print array elements
+void shiftArray(/* in */ const int[], /* iOut */ int[], /* in */ int);    // Function that shifts each element in the array by one to the right
+void reverseArray(/* in */ const int[], /* inOut */ int[], /* in */ int); // Function to reverse order of array ** size of array must be odd **
+
+int main()
+{
+    // Declare and initialize variables
+    const int ARRAY_SIZE = 7;                       // Size of array
+    int array1[ARRAY_SIZE] = {4, 7, 2, 8, 1, 3, 0}; // Array with initialize size and elements
+    int array2[ARRAY_SIZE];                         // Array to be used for function assignments
+
+    // Display message and call functions
+    cout << "Output of array1 with numbers\n";
+    showArray(array1, ARRAY_SIZE);
+
+    cout << "Output of array2 with elements shifted by one to the right\n";
+    shiftArray(array1, array2, ARRAY_SIZE);
+
+    cout << "Output of array2 in reverse order\n";
+    reverseArray(array1, array2, ARRAY_SIZE);
+}
+
+//***********************
+// Function definitions *
+//***********************
+
+//***********************************************
+// Function to print elements in array.         *
+// Simply iterates through the array and prints *
+// each element.                                *
+//***********************************************
+void showArray(/* in */ int someArray[], /* in */ int size)
+{
+    // Pre:     Creates a for loop to iterate through array
+    // Post:    Prints each element in array and an endl after loop is complete
+    for (unsigned index = 0; index < size; index++)
+    {
+        cout << someArray[index] << " ";
+    }
+    cout << "\n";
+}
+
+//***********************************************
+// Function that shifts each element in an      *
+// array one unit to the right.                 *
+// This function takes to array arguments and   *
+// Take the refArray and someArray and assign   *
+// someArray with the refArray[n - 1].          *
+// someArray[0] is assigned with the last value *
+// of refArray depicting the array has looped   *
+// to the first subscript. Loop is repeated     *
+// until array is completed and all values have *
+// been assigned, "rotating" array one to the   *
+// right.                                       *
+//***********************************************
+void shiftArray(/* in */ const int refArray[], /* inOut */ int someArray[], /* in */ int size)
+{
+    // Pre:     Take the refArray and someArray and assign someArray with the refArray[n-1]
+    // Post:    someArray[0] is assigned with the last value of refArray depicting the array has looped to the first subscript.
+    //          Loop is repeated until array is completed and all values have been assigned, "rotating" array one to the right.
+    // Declare variables
+
+    // Being loop
+    for (unsigned index = 0; index < size; index++)
+    {
+        if (index == 0)
+            someArray[index] = refArray[size - 1];
+        else
+            someArray[index] = refArray[index - 1];
+    }
+    // Print array
+    showArray(someArray, size);
+}
+
+//************************************************
+// Function to reverse the order of the array.   *
+// This function finds the middle subscript and  *
+// assigns the elements next the middle with the *
+// refArray element. Each iteration increases,   *
+// and decreases the respective positions from   *
+// the middle and assigns the elements from the  *
+// refArray to the second.                       *
+//************************************************
+void reverseArray(/* in */ const int refArray[], /* inOut */ int someArray[], /* in */ int size)
+{
+    // Pre:    Variable is created and initialize with the middle subscript and is used for the for loop
+    // Post:   The for loop swaps the elements on both sides of the middle position. After each iteration
+    //         each position is incremented and decremented further from the middle until the loop completes.
+    // Declare variables
+    int middle = size / 2;
+
+    // Begin loop
+    for (unsigned index = 0; index <= middle; index++)
+    {
+        someArray[middle - index] = refArray[middle + index];
+        someArray[middle + index] = refArray[middle - index];
+    }
+    // Print array
+    showArray(someArray, size);
+}
---
+++ Thomas_p4b.cpp
@@ -0,0 +1,82 @@
+/*
+Name: Joel Thomas
+Class: CS10A
+Date: April 29, 2020
+Instructor: Sujan Sarkar
+Filename: Thomas_p4b.cpp
+*/
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Project 4a: Collecting banknotes                                                                    //
+// This program takes an amount from the user, calculates the number of banknotes and prints the       //
+// banknotes to user                                                                                   //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+void numBanknotes(/* in */ const int[], /* in */ int, /* in */ int); // Function to calculate the number of banknotes deducted from user amount and print banknotes to user
+
+int main()
+{
+    // Declare variables
+    const int ARRAY_SIZE = 5;                       // Size of array to use
+    int banknotes[ARRAY_SIZE] = {50, 20, 10, 5, 1}; // Array that holds the currency declarations
+    int plussarAmount = 0;                          // Variable for user input
+
+    // Prompt for and get amount
+    cout << "Welcome to Plusplusland.\n"
+         << "Please enter the plussar amount: ";
+    cin >> plussarAmount;
+
+    // Call functions
+    numBanknotes(banknotes, ARRAY_SIZE, plussarAmount); // Calculate the number of banknotes deducted from user amount and print banknotes to user
+}
+
+//***********************
+// Function definitions *
+//***********************
+
+//*********************************************************************
+// Function to print number of banknotes to dispense to user.         *
+// This function will take in the const array to use as a reference.  *
+// It will assign quotient of the division of the array subscript and *
+// amount remaining and print the number of banknotes if quotient is  *
+// greater than 0. Once there is no more amount remaining the loop    *
+// will end.                                                          *
+//*********************************************************************
+void numBanknotes(/* in */ const int refArray[], /* in */ int arraySize, /* in */ int amount)
+{
+    // Pre:     Take the array, size, and user amount and assign the quotient of the remanining amount and array subscript element to a variable.
+    // Post:    The number of banknotes calculated by the quotient is printed along with the element in the refArray. Loop is repeated until the remaining amount is 0.
+    // Declare variables.
+    int amountRemaining = amount,
+        numNotes = 0;
+
+    // Begin loop
+    for (int index = 0; index < arraySize; index++)
+    {
+        // If there is remaning amount, continue
+        if (amountRemaining != 0)
+        {
+            // Assign number of divisions to numNotes
+            numNotes = amountRemaining / refArray[index];
+            // Calculate and assign the remaning amount and continue loop
+            amountRemaining = amountRemaining % refArray[index];
+
+            // If numNotes is greater than 0 the number of respective bills are printed
+            if (numNotes > 0)
+            {
+                for (int counter = 0; counter < numNotes; counter++)
+                {
+                    cout << refArray[index] << " ";
+                }
+            }
+        }
+        // If there is no more remaining, end the loop.
+        else if (amountRemaining == 0)
+        {
+            break;
+        }
+    }
+}
